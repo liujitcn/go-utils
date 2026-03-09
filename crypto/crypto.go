d25f71c3a5ac10452f6b1d2d484033a08704ebc9
210583 <liujun@newcapec.cn>
2026-03-09 22:37:58 +0800

    各种加密算法

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,17 +1,37 @@
 package crypto
 
 import (
-	"golang.org/x/crypto/bcrypt"
+	"errors"
+	"strings"
 )
 
-// HashPassword 加密密码
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-	return string(bytes), err
+// Crypto 密码加解密接口
+type Crypto interface {
+	// Encrypt 加密密码，返回加密后的字符串（包含算法标识和盐值）
+	Encrypt(plainPassword string) (encrypted string, err error)
+
+	// Verify 验证密码是否匹配
+	Verify(plainPassword, encrypted string) (bool, error)
 }
 
-// CheckPasswordHash 校验密码
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
+func NewCrypto(algorithm string) (Crypto, error) {
+	algorithm = strings.ToLower(algorithm)
+	switch algorithm {
+	case "bcrypt":
+		return NewBCryptCrypto(), nil
+	case "pbkdf2":
+		return NewPBKDF2Crypto(), nil
+	case "argon2":
+		return NewArgon2Crypto(), nil
+	case "sha256":
+		return NewSHA256Crypto(), nil
+	case "sha512":
+		return NewSHA512Crypto(), nil
+	case "ecdsa":
+		return NewECDSACrypto()
+	case "ecdh":
+		return NewECDHCrypto()
+	default:
+		return nil, errors.New("不支持的加密算法")
+	}
 }
